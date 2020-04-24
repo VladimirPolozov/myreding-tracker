5dfa3c0788a20d449b984bb9255cbe616a4465ac
VladimirPolozov <z2005q.polozov@yandex.ru>
2020-04-24 13:08:07 +0700

    my first commit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from flask import request
 from flask_login import current_user
+from pip._internal.vcs import git
+
 from __init__ import *
 # для создания сессии
 from data import db_session
@@ -16,6 +19,18 @@ from blueprints.add_book import add_book
 from blueprints.book import book
 from blueprints.delete_book import delete_book
 from blueprints.api import get_statics
+
+
+@app.route('/update', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('https://github.com/VladimirPolozov/myreding-tracker')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+
 
 if __name__ == '__main__':
     # Создание БД
