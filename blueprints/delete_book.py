ad7ec542e268c57437f92c3ef0fb5c6655f70aea
VladimirPolozov <z2005q.polozov@yandex.ru>
2020-04-28 18:24:34 +0700

    redirect from my_book to books when book delete

--- blueprints/delete_book.py
+++ blueprints/delete_book.py
@@ -29,4 +29,4 @@ def delete_book(book_id):
             Relationship.book_id == book_id).delete()
         session.commit()
         # сообщаем пользователю, что мы удалили его книгу
-        return redirect(url_for('books_page.books', message='Книга удалена')
+        return redirect(url_for('books_page.books', message='Книга удалена'))
